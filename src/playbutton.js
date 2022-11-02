5379b02de1703a1d1d2ad6e165a8b2b54c5054d2
Mathew A. Kramer <kramer.mathew.a@gmail.com>
2022-11-02 15:47:25 -0400

    Clean up playbutton

--- src/playbutton.js
+++ src/playbutton.js
@@ -15,39 +15,24 @@ class PlayButton {
   draw(interval) {
   
     let buttonFill = this.color;
-    let triangleFill = [0, 0, 0];
     let img = playOff;
     
-    if(playing) {
-        //console.log("In play button: " + interval);
-        if(interval <= 100) {
-            //buttonFill = [255, 255, 255];
-            //triangleFill = this.color;
-            img = playBeat;
-        }
+    if(playing && interval <= 100) {
+      img = playBeat;
     }
     
     fill(buttonFill);
     push();
     noStroke();
-    ellipse(playButton.x, playButton.y, playButton.c, playButton.c);
+    ellipse(this.x, this.y, this.c, this.c);
     pop();
     
-    image(img, playButton.x - playButton.c/2 * 1.25, playButton.y - playButton.c/2 * 1.25, playButton.c * 1.25, playButton.c * 1.25);
-
-
-    // fill(triangleFill);
-    // triangle(playButton.x - (playButton.c/6), 
-    //          playButton.y - (playButton.c/4), 
-    //          playButton.x - (playButton.c/6), 
-    //          playButton.y + (playButton.c/4), 
-    //          playButton.x + (playButton.c/3.5), 
-    //          playButton.y)
+    image(img, this.x - this.c/2 * 1.25, this.y - this.c/2 * 1.25, this.c * 1.25, this.c * 1.25);
   }
 
-  resize() {
+  resize(height) {
     this.x = canvasXStep * canvasXDiv * 5/10;
-    this.y = canvasYStep * 3.25 + instAreaHeight;
+    this.y = canvasYStep * 3.25 + height;
     this.c = canvasXStep;
   }
 }
